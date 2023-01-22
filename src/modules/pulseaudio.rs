605c1d57bf00d6251dcde046f498f7a621f4a4d0
toheu <aoeuhoeu>
2023-01-22 11:50:36 +0530

    pulse audio print for only necessory events

--- src/modules/pulseaudio.rs
+++ src/modules/pulseaudio.rs
@@ -1,5 +1,10 @@
 use anyhow::{anyhow, Result};
-use std::{rc::Rc, sync::Mutex, thread::sleep, time::Duration};
+use std::{
+    rc::Rc,
+    sync::{Arc, Mutex},
+    thread::sleep,
+    time::Duration,
+};
 
 use pulse::{
     context::Context,
@@ -25,8 +30,11 @@ impl Connection {
         let mnlp = Mainloop::new().unwrap();
         for _ in 0..10 {
             let mut cnxt = Context::new(&mnlp, "pfui_listener").unwrap();
-            if cnxt.connect(None, pulse::context::FlagSet::NOAUTOSPAWN, None).is_ok() {
-                return Ok(Self { cnxt, mnlp })
+            if cnxt
+                .connect(None, pulse::context::FlagSet::NOAUTOSPAWN, None)
+                .is_ok()
+            {
+                return Ok(Self { cnxt, mnlp });
             }
             sleep(Duration::from_secs(timeout));
         }
@@ -56,6 +64,17 @@ impl Connection {
     }
 }
 
+fn print_state(avg: u32, state: bool) {
+    let percent = u32::try_from((f64::from(avg) / f64::from(0x10000) * 100.0).round() as i64);
+    match percent {
+        Ok(percent) => crate::print(&Some(Data {
+            volume: percent,
+            muted: state,
+        })),
+        Err(_) => crate::print::<Data>(&None),
+    }
+}
+
 pub struct PulseAudio {}
 
 impl Module for PulseAudio {
@@ -65,6 +84,8 @@ impl Module for PulseAudio {
     }
 
     fn start(&mut self, timeout: u64) -> Result<()> {
+        let default_sink_index: Arc<std::sync::RwLock<Option<u32>>> =
+            Arc::new(std::sync::RwLock::new(None));
         let mut conn = self.connect(timeout)?;
         if conn.connect().is_err() {
             return Err(anyhow!("Error establishing connection"));
@@ -77,43 +98,41 @@ impl Module for PulseAudio {
         // One possible way to solve this would be to output the data for each available sink and
         // let the user figure out which one to use.
         let introspector = Rc::new(Mutex::new(conn.cnxt.introspect()));
+        // print the data for initialization
         {
             let i = Rc::clone(&introspector);
             let d = i.lock().unwrap();
-            d.get_sink_info_list(|list| {
+            let index_c = default_sink_index.clone();
+            d.get_sink_info_by_name("@DEFAULT_SINK@", move |list| {
                 if let pulse::callbacks::ListResult::Item(sink) = list {
-                    let avg = sink.volume.avg().0;
-                    let percent =
-                        u32::try_from((f64::from(avg) / f64::from(0x10000) * 100.0).round() as i64);
-                    match percent {
-                        Ok(percent) => crate::print(&Some(Data {
-                            volume: percent,
-                            muted: sink.mute,
-                        })),
-                        Err(_) => crate::print::<Data>(&None),
+                    if let Ok(mut sink_index) = index_c.write() {
+                        *sink_index = Some(sink.index);
                     }
+                    print_state(sink.volume.avg().0, sink.mute);
                 }
             });
         }
         conn.cnxt
-            .set_subscribe_callback(Some(Box::new(move |_facility, _operation, index| {
-                let i = Rc::clone(&introspector);
-                let d = i.lock().unwrap();
-                d.get_sink_info_by_index(index, |s| {
-                    if let pulse::callbacks::ListResult::Item(item) = s {
-                        let avg = item.volume.avg().0;
-                        let percent = u32::try_from(
-                            (f64::from(avg) / f64::from(0x10000) * 100.0).round() as i64,
-                        );
-                        match percent {
-                            Ok(percent) => crate::print(&Some(Data {
-                                volume: percent,
-                                muted: item.mute,
-                            })),
-                            Err(_) => crate::print::<Data>(&None),
+            .set_subscribe_callback(Some(Box::new(move |facility, _operation, index| {
+                if let Ok(sink_index_guard) = default_sink_index.read() {
+                    if let Some(sink_index) = *sink_index_guard {
+                        if index == sink_index {
+                            let i = Rc::clone(&introspector);
+                            let d = i.lock().unwrap();
+                            if let Some(pulse::context::subscribe::Facility::Sink) = facility {
+                                // eprintln!("Event facility: {facility:?}, _operation: {_operation:?}");
+                                d.get_sink_info_by_index(index, |s| {
+                                    if let pulse::callbacks::ListResult::Item(item) = s {
+                                        print_state(item.volume.avg().0, item.mute);
+                                    }
+                                });
+                            }
                         }
+                    } else {
+                        eprintln!("Error: Failed to get default sink");
+                        std::process::exit(1);
                     }
-                });
+                }
             })));
         match conn.mnlp.run() {
             Ok(_retval) => Ok(()),
